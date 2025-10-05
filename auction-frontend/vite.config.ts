636c6c0937919a408cce38844754f82165f721c6
NullByte3 <122549857+NullByte3@users.noreply.github.com>
2025-10-05 16:12:21 +0300

    feat: support for docker

--- auction-frontend/vite.config.ts
+++ auction-frontend/vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 import { resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
 
-// https://vitejs.dev/config/
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = resolve(__filename, '..');
+
 export default defineConfig({
   plugins: [
     TanStackRouterVite({ autoCodeSplitting: true }),
